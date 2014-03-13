1db84015807ebcc0c02cd6dc629c88457abdfe82
githubUser919 <duk444@gmail.com>
2014-03-13 17:00:59 +0000

    Completed instructions at http://www.johnpapa.net/wp-content/sites/htngz/readme/readme.html
    ...but we now have an error in the console, as per the comment from 'Vincent Apa' here: http://www.johnpapa.net/hot-towel-angular/#comment-1107249138

--- CC/CC.Web/app/app.js
+++ CC/CC.Web/app/app.js
@@ -12,11 +12,14 @@
         'common.bootstrap', // bootstrap dialog wrapper functions
 
         // 3rd Party Modules
+        'breeze.angular.q',  // tells breeze to use $q instead of Q.js
+        'breeze.directives', // breeze validation directive (zValidate)
         'ui.bootstrap'      // ui-bootstrap (ex: carousel, pagination, dialog)
     ]);
     
-    // Handle routing errors and success events
-    app.run(['$route',  function ($route) {
-            // Include $route to kick start the router.
-        }]);        
+    // Handle routing errors and success events.
+    // Trigger breeze configuration
+    app.run(['$route', 'config.breeze', function ($route) {
+        // Include $route to kick start the router.
+    }]);
 })();
